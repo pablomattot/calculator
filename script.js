6b9ab7ca0cd46bf3ed2edc0e170352b6f5755be2
Pablo Mattot <mattotpablo@gmail.com>
2021-09-18 18:10:37 +0200

    Created functions to get key input and change the display of calculator

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
-function add(a,b) {
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a,b) {
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply(a,b) {
+function multiply(a, b) {
     return a * b;
 }
 
-function divide(a,b) {
+function divide(a, b) {
     return a / b;
 }
 
-function operate(operator,a,b) {
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b)
     switch (operator) {
         case '+':
-            return add(a,b);
+            return add(a, b);
         case '-':
-            return subtract(a,b);
+            return subtract(a, b);
         case '*':
-            return multiply(a,b);
+            return multiply(a, b);
         case '/':
-            return divide(a,b);
+            return divide(a, b);
     }
+}
+
+const screenInput = document.querySelector("#input");
+const keyboard = document.querySelector(".keyboard");
+const keys = Array.from(keyboard.children);
+
+let displayValue = "";
+
+keys.forEach(key => {
+    key.addEventListener('click', key => {
+        getKey(key)
+        changeDisplay();
+    })
+})
+
+function getKey(key) {
+    displayValue += key.target.textContent;
+}
+
+function changeDisplay() {
+    screenInput.textContent = displayValue;
 }
