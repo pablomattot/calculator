92c698dc358f93a7f7f110e7c366563754e92b32
Pablo Mattot <mattotpablo@gmail.com>
2021-09-19 12:07:09 +0200

    Added comments to javascript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Math functions
 function add(a, b) {
     return a + b;
 }
@@ -29,6 +30,7 @@ function operate(operator, a, b) {
     }
 }
 
+// Query Selectors
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const displayValue = document.querySelector("#display");
@@ -41,6 +43,9 @@ let temporaryStorage = "0";
 let numberArray = [];
 let result;
 
+// Add event listener to all the numbers and decimal sign
+// onclick verify whether the number already is a decimal. If true, you can't add another dot
+// Store the number in the temporary storage variable send it to getInput
 numbers.forEach(number => {
     number.addEventListener('click', number => {
         if (number.target.textContent === ".") {
@@ -57,6 +62,10 @@ numbers.forEach(number => {
     });
 });
 
+// Add an eventlistener to each operator
+// if an action (operator) has already been assigned, run the calculate function
+// Send the given opertor to getInput and store it in the action variable
+// Run storeNumber()
 operators.forEach(operator => {
     operator.addEventListener('click', operator => {
         if (action) {
@@ -69,21 +78,29 @@ operators.forEach(operator => {
     });
 });
 
+// Add the input to the display
 function getInput(key) {
     displayValue.textContent += `${key.target.textContent}`;
 }
 
+// Store the temporarily stored number in an array to allow another number to be stored
 function storeNumber() {
     numberArray.push(temporaryStorage);
     temporaryStorage = "";
 }
 
+// If the requirements (numbers inputted and operator chosen) are met, run the calculate function
 process.addEventListener('click', () => {
     if (numberArray[0] && action && temporaryStorage) {
         calculate();
     } else { console.log("insert numbers and operator first") }
 });
 
+// Set the result variable to the result of the operate function
+// Update the display
+// Store the result in temporarystorage so it can be used again if necessary
+// Empty the action variable to allow another to be picked
+// Empty the numberArray to allow new numbers to be stored
 function calculate() {
     storeNumber();
 
@@ -98,11 +115,13 @@ function calculate() {
     numberArray.splice(0, numberArray.length);
 }
 
+// Change the output display to the result and clear the input display
 function updateDisplay() {
     outputDisplay.textContent = `${result}`;
     displayValue.textContent = "";
 }
 
+// Hard clear everything to reset the calculator
 reset.addEventListener('click', clearAll);
 
 function clearAll() {
