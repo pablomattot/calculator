9bc3d0d1fdcd5a4de7c6e4479aa31ce1786f75cf
Pablo Mattot <mattotpablo@gmail.com>
2021-09-20 11:59:51 +0200

    Refactored javascript code and cleaned it up

--- script.js
+++ script.js
@@ -31,105 +31,99 @@ function operate(operator, a, b) {
 }
 
 // Query Selectors
-const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operator");
-const displayValue = document.querySelector("#display");
-const process = document.querySelector("#equals");
+const keysArray = Array.from(document.querySelector(".keyboard").children);
+const inputDisplay = document.querySelector("#display");
 const outputDisplay = document.querySelector("#output");
-const reset = document.querySelector("#reset")
 
-let action = "";
-let temporaryStorage = "0";
+let temporaryStorage = "";
 let numberArray = [];
+let operator = "";
 let result;
 
-// Add event listener to all the numbers and decimal sign
-// onclick verify whether the number already is a decimal. If true, you can't add another dot
-// Store the number in the temporary storage variable send it to getInput
-numbers.forEach(number => {
-    number.addEventListener('click', number => {
-        if(result) {result = null;temporaryStorage = "0";}
-        if (number.target.textContent === ".") {
-            if (temporaryStorage.includes(".")) {
-                console.log("already decimal");
-            } else {
-                getInput(number);
-                temporaryStorage += number.target.textContent;
+// Link functions to correct keys
+keysArray.forEach(key => {
+    key.addEventListener('click', (key) => {
+        if (key.target.classList.contains("number")) {
+            // If a result is already present, clear everything.
+            // User can start new expression without clicking RESET
+            if (result) { clearAll(); }
+            // If the number already is a decimal, stop user from adding more '.'
+            if (!checkDecimal(key)) {
+                temporaryStorage += getInput(key);
+                updateDisplay(key);
             }
-        } else {
-            getInput(number);
-            temporaryStorage += number.target.textContent;
+        } else if (key.target.classList.contains("operator")) {
+            // Only allow operators if a number is already present
+            if (numberArray[0] || temporaryStorage) {
+                operator = getInput(key);
+                storeNumber();
+                result = null;
+                updateDisplay(key);
+            } else {console.log("Enter a number first")}
+        } else if (key.target.id === "equals") {
+            calculateExpression();
+            updateDisplay(key);
+        } else if (key.target.id === "reset") {
+            clearAll();
         }
-    })
-});
-
-// Add an eventlistener to each operator
-// if an action (operator) has already been assigned, run the calculate function
-// Send the given opertor to getInput and store it in the action variable
-// Run storeNumber()
-operators.forEach(operator => {
-    operator.addEventListener('click', operator => {
-        if (action) {
-            calculate();
-        }
-
-        getInput(operator);
-        action = operator.target.textContent;
-        storeNumber();
     });
-});
+})
 
-// Add the input to the display
+// Get input
 function getInput(key) {
-    displayValue.textContent += `${key.target.textContent}`;
+    return key.target.textContent;
 }
 
-// Store the temporarily stored number in an array to allow another number to be stored
+// Store first number
 function storeNumber() {
     numberArray.push(temporaryStorage);
     temporaryStorage = "";
 }
 
-// If the requirements (numbers inputted and operator chosen) are met, run the calculate function
-process.addEventListener('click', () => {
-    if (numberArray[0] && action && temporaryStorage) {
-        calculate();
-    } else { console.log("insert numbers and operator first") }
-});
+// Display input/output
+function updateDisplay(key) {
+    if (key.target.classList.contains("number") || key.target.classList.contains("operator")) {
+        inputDisplay.textContent += getInput(key);
 
-// Set the result variable to the result of the operate function
-// Update the display
-// Store the result in temporarystorage so it can be used again if necessary
-// Empty the action variable to allow another to be picked
-// Empty the numberArray to allow new numbers to be stored
-function calculate() {
-    storeNumber();
+    } else {
+        console.log("not a number or operator");
+    }
+    if (result) {
+        outputDisplay.textContent = result;
+    }
+}
 
+// Evaluate expression
+function calculateExpression() {
     let a = numberArray[0];
-    let b = numberArray[1];
-
-    result = operate(action, a, b);
-    updateDisplay();
+    let b = temporaryStorage;
+    result = roundNumber(operate(operator, a, b));
 
     temporaryStorage = result;
-    action = "";
     numberArray.splice(0, numberArray.length);
+
+    return roundNumber(operate(operator, a, b));
 }
 
-// Change the output display to the result and clear the input display
-function updateDisplay() {
-    outputDisplay.textContent = `${result}`;
-    displayValue.textContent = "";
+// Round Result
+function roundNumber(number) {
+    return +number.toFixed(6);
 }
 
-// Hard clear everything to reset the calculator
-reset.addEventListener('click', clearAll);
+function checkDecimal(key) {
+    if (key.target.textContent === ".") {
+        if (temporaryStorage.includes(".")) {
+            console.log("already decimal");
+            return true;
+        }
+    }
+}
 
+// Reset calculator
 function clearAll() {
-    temporaryStorage = "0";
-    numberArray.splice(0, numberArray.length);
-    action = "";
     result = null;
-    displayValue.textContent = "";
+    temporaryStorage = "";
+    numberArray.splice(0, numberArray.length);
+    inputDisplay.textContent = "";
     outputDisplay.textContent = "";
 }
